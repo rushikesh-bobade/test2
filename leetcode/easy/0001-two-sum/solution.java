08a6041b304d74f8db0402d2e48b9d662a02838b
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-07-08 19:27:08 +0530

    ✅ Two Sum | Easy | Java | Runtime: 0 ms | Memory: 8.4 MB | 2026-07-08

--- leetcode/easy/0001-two-sum/solution.java
+++ leetcode/easy/0001-two-sum/solution.java
@@ -1,11 +1,16 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = {}
-
-        for i, num in enumerate(nums):
-            complement = target - num
-
-            if complement in seen:
-                return [seen[complement], i]
-
-            seen[num] = i
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int>mpp;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            int num=nums[i];
+            int moreNeeded = target-num;
+            if(mpp.find(moreNeeded)!=mpp.end()){
+                return{mpp[moreNeeded],i};
+            }
+            mpp[num]= i;
+        }
+        return{-1,-1};
+    }
+};
